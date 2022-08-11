87afac9f9ef3f0dd1a6d72da7aba40e4464f08e4
Linah Muhonja <linahmuhonjaimbari@gmail.com>
2022-08-11 15:21:19 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+// const pple = [ 'Guadalupe', 'Ollie', 'Aki' ];
+function writeCards(pple, event){
+    const watu = [];
+    for(let i = 0; i < pple.length; i++){
+        watu.push(`Thank you, ${pple[i]}, for the wonderful ${event} gift!`);
+    }
+    return watu ;
+}
+
+function countDown(num) {
+
+    while(num >= 0){
+        console.log(num);
+       num--;
+    }
+    //console.log (num);
+}
+countDown(10);
+
+        
